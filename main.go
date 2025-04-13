d71ee1a4c893049e3c74407d6f282c4d28a2213e
Aristide <aristide.bamazi@imsp-uac.org>
2025-04-13 11:38:51 +0000

    Let the system react to system signal for graceful shutdown

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"os"
+	"os/signal"
 	"time"
 
 	_ "github.com/mattn/go-sqlite3"
@@ -29,6 +31,16 @@ func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
+	sigChan := make(chan os.Signal, 3)
+	signal.Notify(sigChan, os.Interrupt, os.Kill)
+
+	// Cancel the context on system interrupt signal
+	// reception
+	go func() {
+		<-sigChan
+		cancel()
+	}()
+
 	// Ping the datasource to make sure everything it is available
 	if err := Ping(ctx, pool); err != nil {
 		log.Fatal(err)
@@ -36,6 +48,7 @@ func main() {
 }
 
 func Ping(ctx context.Context, pool *sql.DB) error {
+	time.Sleep(time.Second)
 	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
 	defer cancel()
 
